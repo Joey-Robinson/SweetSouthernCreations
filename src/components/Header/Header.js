8a086efc5b4e53d42b2f321752cd37a78834bc66
joeyrobinson <joeyrobinsondev@gmail.com>
2018-03-16 18:20:50 -0500

    working on this for a few weeks

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = () => (
+  <div className="Header">
+    <div>
+      <h1>
+        Sweet Southern Creations
+      </h1>
+    </div>
+  </div>
+)
+
+export default Header
